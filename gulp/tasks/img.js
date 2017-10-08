be526b49b892fd1afe4af1c20cc665e5e4321060
evlads <lvivua100@gmail.com>
2017-10-08 20:22:59 +0300

    Add favicon

--- gulp/tasks/img.js
+++ gulp/tasks/img.js
@@ -1,11 +1,11 @@
 module.exports = function() {
     $.gulp.task('img:dev', () => {
-        return $.gulp.src('./dev/static/img/**/*.{png,jpg,gif,svg}')
+        return $.gulp.src('./dev/static/img/**/*.*') //*.{png,jpg,gif,svg}
             .pipe($.gulp.dest('./build/static/img/'));
     });
 
     $.gulp.task('img:build', () => {
-        return $.gulp.src('./dev/static/img/**/*.{png,jpg,gif,svg}')
+        return $.gulp.src('./dev/static/img/**/*.*') //*.{png,jpg,gif,svg}
             // .pipe($.gp.tinypng(YOUR_API_KEY))
             .pipe($.gulp.dest('./build/static/img/'));
     });
